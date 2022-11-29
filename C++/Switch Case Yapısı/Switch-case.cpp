69b554faf1df66e36061b3cf9c117190ce6d1748
Erol Dalkılıç <eroldalg@gmail.com>
2022-11-29 15:57:43 +0300

    switch-case

---
+++ C++/Switch Case Yapısı/Switch-case.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    a= 1;
+    switch(a)
+    {
+    	case 1 : cout << '1';
+    	break;
+    	case 2 : cout << '2';
+    	break;
+    	default:
+    		cout <<"diger durumlar"<<endl;
+	}
+}
