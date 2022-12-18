fe883875e48c1ad8fd404bf81fb0e0100731f85f
Erol Dalkılıç <eroldalg@gmail.com>
2022-12-18 17:30:19 +0300

    Dowhile

---
+++ C++/Dowhile Döngü Yapısı/Dowhile.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+     int a;//tipi,ismi
+    a = 1;//ilk deger , initialization
+    while(a<10)
+    {
+    	cout << "merhaba " << a <<endl;
+    	a++;
+    	//a=100;
+    	
+	}
+	cout << a <<endl;
+		 
+	for(int i = 1; i<10;i++)
+	{
+		cout << "udemy "<< i <<endl;
+	}
+		
+	cout << a <<endl;
+	
+	int x = 1;
+	do
+	{
+		cout << "Erol "<< x <<endl;
+		x++;
+	}
+	while(x<10);
+	cout << a <<endl;
+
+	
+	
+}
