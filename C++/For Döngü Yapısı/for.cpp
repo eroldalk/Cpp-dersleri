42237e332c6f5b336a2b2cf50d73cd30707599b6
Erol Dalkılıç <eroldalg@gmail.com>
2022-12-18 17:19:30 +0300

    For

---
+++ C++/For Döngü Yapısı/for.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+     int a;//tipi,ismi
+    a = 1;//ilk deger , initialization
+    while(a<10)
+    {
+    	cout << "merhaba " << a <<endl;
+    	a++;
+    	//a=100;
+    	
+	}
+	cout << a <<endl;
+		 
+	for(int i = 0; i<10;i++)
+	{
+		cout << "udemy "<<endl;
+	}
+		
+	cout << a <<endl;
+	
+	
+}
