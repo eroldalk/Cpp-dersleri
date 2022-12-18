6b2e232b86fa942f2397cc6c197148ff611c98e0
Erol Dalkılıç <eroldalg@gmail.com>
2022-12-18 17:05:34 +0300

    While

---
+++ C++/While Döngü Yapısı/while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;//tipi,ismi
+    a = 1;//ilk deger , initialization
+    while(a<10)
+    {
+    	cout << "merhaba " << a <<endl;
+    	a++;
+    	//a=100;
+    	
+	}
+	cout << a <<endl;
+}
